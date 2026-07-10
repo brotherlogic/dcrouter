4449edcaf4850544da307be91210e70a5e7e4a09
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-07-09 18:05:47 -0700

    Adds startup shell (#18)
    Co-authored-by: Brotherlogic Automation <brotherlogic-automation@gmail.com>

---
+++ start-tmux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ensure the 'dcrouter' session exists
+if ! tmux has-session -t dcrouter 2>/dev/null; then
+  # Create a new session named 'dcrouter', detached
+  cd /workspaces/dcrouter
+  tmux new-session -d -s dcrouter
+fi
